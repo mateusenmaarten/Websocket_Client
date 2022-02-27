93d403f090d33e003b268aa61588bd1550f9972b
Maarten Mateusen <mateusenmaarten@hotmail.com>
2022-02-27 20:34:33 +0100

    Add project files.

---
+++ Websocket_Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using WebSocketSharp.NetCore;
+
+using (WebSocket ws = new WebSocket("ws://localhost:4200/EchoAll"))
+{
+    Console.WriteLine("Input Player name here: ");
+    var playerName = Console.ReadLine();
+    ws.OnMessage += Ws_OnMessage;
+
+    ws.Connect();
+    ws.Send(playerName);
+
+    Console.ReadKey();
+}
+
+void Ws_OnMessage(object? sender, MessageEventArgs e)
+{
+    Console.WriteLine($"The server responded with: {e.Data}");
+}
+
