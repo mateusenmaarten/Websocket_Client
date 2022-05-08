3ac60b4315fde48a4bb89d026a09d3794ff9a85c
Maarten Mateusen <mateusenmaarten@hotmail.com>
2022-05-08 15:14:48 +0200

    Display mainMenu

--- Websocket_Client/Program.cs
+++ Websocket_Client/Program.cs
@@ -4,18 +4,32 @@ using WebSocketSharp.NetCore;
 
 using (WebSocket ws = new WebSocket("ws://localhost:4200/CardsAgainstHumanity"))
 {
-    var nameMessage = JsonSerializer.Serialize(new Message_Login(GetUserName()));
+    var loginMessage = new Message_Login(GetUserName());
+    var rawMessageContent = JsonSerializer.Serialize(loginMessage);
+    var message = JsonSerializer.Serialize(new RawMessage(loginMessage.MessageType, rawMessageContent));
     ws.OnMessage += Ws_OnMessage;
 
     ws.Connect();
-    ws.Send(nameMessage);
+    ws.Send(message);
 
     Console.ReadKey();
 }
 
 void Ws_OnMessage(object? sender, MessageEventArgs e)
 {
-    Console.WriteLine(e.Data);
+    var message = JsonSerializer.Deserialize<RawMessage>(e.Data);
+
+    var messageType = message.MessageType;
+
+    switch (messageType)
+    {
+        case "Message_DisplayMainMenu":
+            Message_DisplayMainMenu menu = JsonSerializer.Deserialize<Message_DisplayMainMenu>(message.MessageContent);
+            Console.WriteLine(menu.Menu);
+            break;
+        default:
+            break;
+    }
 }
 
 string GetUserName()
